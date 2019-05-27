6a3b9890c47482e3ff024ecd0e70dea90f29e8bd
Jiří Tarašovič <jiri.tarasovic@seznam.cz>
2019-05-27 21:24:19 +0800

    BE: Add Mongo Model Mock

---
+++ server/test/mocks/mongo-model.mock.ts
@@ -0,0 +1,8 @@
+export function MongoModelMock() {
+    this.mockFunctions = {
+        exec: jasmine.createSpy('execSpy'),
+        save: jasmine.createSpy('saveSpy'),
+    }
+    this.find = jasmine.createSpy('findSpy').and.returnValue(this.mockFunctions.exec);
+    this.create = jasmine.createSpy('constructorSpy').and.returnValue(this.mockFunctions.save);
+};
